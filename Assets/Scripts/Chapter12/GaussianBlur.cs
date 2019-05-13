1c985c7aa19aefb97f715a82300f3138eb78a980
庭豪 胡 <652157227@qq.com>
2019-05-13 20:06:17 +0800

    Gaussian Blur learn

--- Assets/Scripts/Chapter12/GaussianBlur.cs
+++ Assets/Scripts/Chapter12/GaussianBlur.cs
@@ -1,100 +1,108 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GaussianBlur : PostEffectsBase {
+public class GaussianBlur : PostEffectsBase
+{
 
-	public Shader gaussianBlurShader;
-	private Material gaussianBlurMaterial = null;
+    public Shader gaussianBlurShader;
+    private Material gaussianBlurMaterial = null;
 
-	public Material material {  
-		get {
-			gaussianBlurMaterial = CheckShaderAndCreateMaterial(gaussianBlurShader, gaussianBlurMaterial);
-			return gaussianBlurMaterial;
-		}  
-	}
+    public Material material
+    {
+        get
+        {
+            gaussianBlurMaterial = CheckShaderAndCreateMaterial(gaussianBlurShader, gaussianBlurMaterial);
+            return gaussianBlurMaterial;
+        }
+    }
 
-	// Blur iterations - larger number means more blur.
-	[Range(0, 4)]
-	public int iterations = 3;
-	
-	// Blur spread for each iteration - larger value means more blur
-	[Range(0.2f, 3.0f)]
-	public float blurSpread = 0.6f;
-	
-	[Range(1, 8)]
-	public int downSample = 2;
-	
-	/// 1st edition: just apply blur
-//	void OnRenderImage(RenderTexture src, RenderTexture dest) {
-//		if (material != null) {
-//			int rtW = src.width;
-//			int rtH = src.height;
-//			RenderTexture buffer = RenderTexture.GetTemporary(rtW, rtH, 0);
-//
-//			// Render the vertical pass
-//			Graphics.Blit(src, buffer, material, 0);
-//			// Render the horizontal pass
-//			Graphics.Blit(buffer, dest, material, 1);
-//
-//			RenderTexture.ReleaseTemporary(buffer);
-//		} else {
-//			Graphics.Blit(src, dest);
-//		}
-//	} 
+    // Blur iterations - larger number means more blur.
+    [Range(0, 4)]
+    public int iterations = 3;
 
-	/// 2nd edition: scale the render texture
-//	void OnRenderImage (RenderTexture src, RenderTexture dest) {
-//		if (material != null) {
-//			int rtW = src.width/downSample;
-//			int rtH = src.height/downSample;
-//			RenderTexture buffer = RenderTexture.GetTemporary(rtW, rtH, 0);
-//			buffer.filterMode = FilterMode.Bilinear;
-//
-//			// Render the vertical pass
-//			Graphics.Blit(src, buffer, material, 0);
-//			// Render the horizontal pass
-//			Graphics.Blit(buffer, dest, material, 1);
-//
-//			RenderTexture.ReleaseTemporary(buffer);
-//		} else {
-//			Graphics.Blit(src, dest);
-//		}
-//	}
+    // Blur spread for each iteration - larger value means more blur
+    [Range(0.2f, 3.0f)]
+    public float blurSpread = 0.6f;
 
-	/// 3rd edition: use iterations for larger blur
-	void OnRenderImage (RenderTexture src, RenderTexture dest) {
-		if (material != null) {
-			int rtW = src.width/downSample;
-			int rtH = src.height/downSample;
+    [Range(1, 8)]
+    public int downSample = 2;
 
-			RenderTexture buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
-			buffer0.filterMode = FilterMode.Bilinear;
+    /// 1st edition: just apply blur
+    //	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+    //		if (material != null) {
+    //			int rtW = src.width;
+    //			int rtH = src.height;
+    //			RenderTexture buffer = RenderTexture.GetTemporary(rtW, rtH, 0);
+    //
+    //			// Render the vertical pass
+    //			Graphics.Blit(src, buffer, material, 0);
+    //			// Render the horizontal pass
+    //			Graphics.Blit(buffer, dest, material, 1);
+    //
+    //			RenderTexture.ReleaseTemporary(buffer);
+    //		} else {
+    //			Graphics.Blit(src, dest);
+    //		}
+    //	} 
 
-			Graphics.Blit(src, buffer0);
+    /// 2nd edition: scale the render texture
+    //	void OnRenderImage (RenderTexture src, RenderTexture dest) {
+    //		if (material != null) {
+    //			int rtW = src.width/downSample;
+    //			int rtH = src.height/downSample;
+    //			RenderTexture buffer = RenderTexture.GetTemporary(rtW, rtH, 0);
+    //			buffer.filterMode = FilterMode.Bilinear;
+    //
+    //			// Render the vertical pass
+    //			Graphics.Blit(src, buffer, material, 0);
+    //			// Render the horizontal pass
+    //			Graphics.Blit(buffer, dest, material, 1);
+    //
+    //			RenderTexture.ReleaseTemporary(buffer);
+    //		} else {
+    //			Graphics.Blit(src, dest);
+    //		}
+    //	}
 
-			for (int i = 0; i < iterations; i++) {
-				material.SetFloat("_BlurSize", 1.0f + i * blurSpread);
+    /// 3rd edition: use iterations for larger blur
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            int rtW = src.width / downSample;
+            int rtH = src.height / downSample;
 
-				RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+            RenderTexture buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
+            buffer0.filterMode = FilterMode.Bilinear;
 
-				// Render the vertical pass
-				Graphics.Blit(buffer0, buffer1, material, 0);
+            Graphics.Blit(src, buffer0);
 
-				RenderTexture.ReleaseTemporary(buffer0);
-				buffer0 = buffer1;
-				buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+            for (int i = 0; i < iterations; i++)
+            {
+                material.SetFloat("_BlurSize", 1.0f + i * blurSpread);
 
-				// Render the horizontal pass
-				Graphics.Blit(buffer0, buffer1, material, 1);
+                RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
 
-				RenderTexture.ReleaseTemporary(buffer0);
-				buffer0 = buffer1;
-			}
+                // Render the vertical pass
+                Graphics.Blit(buffer0, buffer1, material, 0);
 
-			Graphics.Blit(buffer0, dest);
-			RenderTexture.ReleaseTemporary(buffer0);
-		} else {
-			Graphics.Blit(src, dest);
-		}
-	}
+                RenderTexture.ReleaseTemporary(buffer0);
+                buffer0 = buffer1;
+                buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+                // Render the horizontal pass
+                Graphics.Blit(buffer0, buffer1, material, 1);
+
+                RenderTexture.ReleaseTemporary(buffer0);
+                buffer0 = buffer1;
+            }
+
+            Graphics.Blit(buffer0, dest);
+            RenderTexture.ReleaseTemporary(buffer0);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
 }
--- Assets/Shaders/Chapter12/Chapter12-GaussianBlur.shader
+++ Assets/Shaders/Chapter12/Chapter12-GaussianBlur.shader
@@ -1,58 +1,103 @@
-﻿Shader "Unlit/Chapter12-GaussianBlur"
+﻿Shader "Unity Shaders Book/Chapter 12/Gaussian Blur" 
 {
-	Properties
+	Properties 
 	{
-		_MainTex ("Texture", 2D) = "white" {}
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_BlurSize ("Blur Size", Float) = 1.0
 	}
-	SubShader
+
+	SubShader 
 	{
-		Tags { "RenderType"="Opaque" }
-		LOD 100
-
-		Pass
+		CGINCLUDE
+		
+		#include "UnityCG.cginc"
+		
+		sampler2D _MainTex;  
+		half4 _MainTex_TexelSize;
+		float _BlurSize;
+		  
+		struct v2f 
 		{
+			float4 pos : SV_POSITION;
+			half2 uv[5]: TEXCOORD0;
+		};
+		  
+		v2f vertBlurVertical(appdata_img v) 
+		{
+			v2f o;
+			o.pos = UnityObjectToClipPos(v.vertex);
+			
+			half2 uv = v.texcoord;
+			
+			o.uv[0] = uv;
+			o.uv[1] = uv + float2(0.0, _MainTex_TexelSize.y * 1.0) * _BlurSize;
+			o.uv[2] = uv - float2(0.0, _MainTex_TexelSize.y * 1.0) * _BlurSize;
+			o.uv[3] = uv + float2(0.0, _MainTex_TexelSize.y * 2.0) * _BlurSize;
+			o.uv[4] = uv - float2(0.0, _MainTex_TexelSize.y * 2.0) * _BlurSize;
+					 
+			return o;
+		}
+		
+		v2f vertBlurHorizontal(appdata_img v) 
+		{
+			v2f o;
+			o.pos = UnityObjectToClipPos(v.vertex);
+			
+			half2 uv = v.texcoord;
+			
+			o.uv[0] = uv;
+			o.uv[1] = uv + float2(_MainTex_TexelSize.x * 1.0, 0.0) * _BlurSize;
+			o.uv[2] = uv - float2(_MainTex_TexelSize.x * 1.0, 0.0) * _BlurSize;
+			o.uv[3] = uv + float2(_MainTex_TexelSize.x * 2.0, 0.0) * _BlurSize;
+			o.uv[4] = uv - float2(_MainTex_TexelSize.x * 2.0, 0.0) * _BlurSize;
+					 
+			return o;
+		}
+		
+		fixed4 fragBlur(v2f i) : SV_Target 
+		{
+			float weight[3] = {0.4026, 0.2442, 0.0545};
+			
+			fixed3 sum = tex2D(_MainTex, i.uv[0]).rgb * weight[0];
+			
+			for (int it = 1; it < 3; it++) {
+				sum += tex2D(_MainTex, i.uv[it*2-1]).rgb * weight[it];
+				sum += tex2D(_MainTex, i.uv[it*2]).rgb * weight[it];
+			}
+			
+			return fixed4(sum, 1.0);
+		}
+		
+		ENDCG
+		
+		ZTest Always 
+		Cull Off 
+		ZWrite Off
+		
+		Pass 
+		{
+			NAME "GAUSSIAN_BLUR_VERTICAL"
+			
 			CGPROGRAM
-			#pragma vertex vert
-			#pragma fragment frag
-			// make fog work
-			#pragma multi_compile_fog
+			  
+			#pragma vertex vertBlurVertical  
+			#pragma fragment fragBlur
+			  
+			ENDCG  
+		}
+		
+		Pass 
+		{  
+			NAME "GAUSSIAN_BLUR_HORIZONTAL"
 			
-			#include "UnityCG.cginc"
-
-			struct appdata
-			{
-				float4 vertex : POSITION;
-				float2 uv : TEXCOORD0;
-			};
-
-			struct v2f
-			{
-				float2 uv : TEXCOORD0;
-				UNITY_FOG_COORDS(1)
-				float4 vertex : SV_POSITION;
-			};
-
-			sampler2D _MainTex;
-			float4 _MainTex_ST;
+			CGPROGRAM  
 			
-			v2f vert (appdata v)
-			{
-				v2f o;
-				o.vertex = UnityObjectToClipPos(v.vertex);
-				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-				UNITY_TRANSFER_FOG(o,o.vertex);
-				return o;
-			}
+			#pragma vertex vertBlurHorizontal  
+			#pragma fragment fragBlur
 			
-			fixed4 frag (v2f i) : SV_Target
-			{
-				// sample the texture
-				fixed4 col = tex2D(_MainTex, i.uv);
-				// apply fog
-				UNITY_APPLY_FOG(i.fogCoord, col);
-				return col;
-			}
 			ENDCG
 		}
-	}
+	} 
+
+	FallBack "Diffuse"
 }
