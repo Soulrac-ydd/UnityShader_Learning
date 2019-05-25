c5c5b66373fd9eb602dc5b7b692a485c4852be1b
庭豪 胡 <652157227@qq.com>
2019-05-25 16:16:25 +0800

    chapter 13
    edge detection with normals and depth

--- Assets/Scripts/Chapter13/EdgeDetectNormalsAndDepth.cs
+++ Assets/Scripts/Chapter13/EdgeDetectNormalsAndDepth.cs
@@ -1,46 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EdgeDetectNormalsAndDepth : PostEffectsBase {
+public class EdgeDetectNormalsAndDepth : PostEffectsBase
+{
 
-	public Shader edgeDetectShader;
-	private Material edgeDetectMaterial = null;
-	public Material material {  
-		get {
-			edgeDetectMaterial = CheckShaderAndCreateMaterial(edgeDetectShader, edgeDetectMaterial);
-			return edgeDetectMaterial;
-		}  
-	}
+    public Shader edgeDetectShader;
+    private Material edgeDetectMaterial = null;
+    public Material material
+    {
+        get
+        {
+            edgeDetectMaterial = CheckShaderAndCreateMaterial(edgeDetectShader, edgeDetectMaterial);
+            return edgeDetectMaterial;
+        }
+    }
 
-	[Range(0.0f, 1.0f)]
-	public float edgesOnly = 0.0f;
+    [Range(0.0f, 1.0f)]
+    public float edgesOnly = 0.0f;
 
-	public Color edgeColor = Color.black;
+    public Color edgeColor = Color.black;
 
-	public Color backgroundColor = Color.white;
+    public Color backgroundColor = Color.white;
 
-	public float sampleDistance = 1.0f;
+    public float sampleDistance = 1.0f;
 
-	public float sensitivityDepth = 1.0f;
+    public float sensitivityDepth = 1.0f;
 
-	public float sensitivityNormals = 1.0f;
-	
-	void OnEnable() {
-		GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
-	}
+    public float sensitivityNormals = 1.0f;
 
-	[ImageEffectOpaque]
-	void OnRenderImage (RenderTexture src, RenderTexture dest) {
-		if (material != null) {
-			material.SetFloat("_EdgeOnly", edgesOnly);
-			material.SetColor("_EdgeColor", edgeColor);
-			material.SetColor("_BackgroundColor", backgroundColor);
-			material.SetFloat("_SampleDistance", sampleDistance);
-			material.SetVector("_Sensitivity", new Vector4(sensitivityNormals, sensitivityDepth, 0.0f, 0.0f));
+    void OnEnable()
+    {
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
+    }
 
-			Graphics.Blit(src, dest, material);
-		} else {
-			Graphics.Blit(src, dest);
-		}
-	}
+    [ImageEffectOpaque]
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            material.SetFloat("_EdgeOnly", edgesOnly);
+            material.SetColor("_EdgeColor", edgeColor);
+            material.SetColor("_BackgroundColor", backgroundColor);
+            material.SetFloat("_SampleDistance", sampleDistance);
+            material.SetVector("_Sensitivity", new Vector4(sensitivityNormals, sensitivityDepth, 0.0f, 0.0f));
+
+            Graphics.Blit(src, dest, material);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
 }
